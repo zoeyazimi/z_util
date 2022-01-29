a2ae30bb0851aa32c67ca6f79db8812dd3fc18f4
zohre azimi <zohre.azimi88@gmail.com>
2022-01-29 17:26:22 +0330

    util package initialized

---
+++ z_util/plotting_default.py
@@ -0,0 +1,8 @@
+"""
+    import matplot lib as plt and use seaborn default
+    for matplotlib plots.
+"""
+import matplotlib.pyplot as plt
+import seaborn as sns
+import matplotlib as mpl
+mpl.style.use('seaborn')
